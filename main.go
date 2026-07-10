d1175ecef854757a7b0e9c82a14d86d42830d58d
vpro3611 <vprov8645@gmail.com>
2026-07-10 18:31:17 +0200

    add: added replay on startup and janitor worker in background for cleaning expired keys every N seconds

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"github.com/vpro3611/gomembase.git/core"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	wal, walErr := core.NewWal("test.wal")
+	if walErr != nil {
+		panic(walErr)
+	}
+
+	defer func(wal *core.Wal) {
+		err := wal.CloseWal()
+		if err != nil {
+			panic(err)
+		}
+	}(wal)
+
+	storage := core.NewStorage(wal)
+
+	loadErr := storage.Load()
+	if loadErr != nil {
+		panic(loadErr)
+	}
+
+	go func() {
+		ticker := time.NewTicker(10 * time.Second)
+		for range ticker.C {
+			storage.CleanupExpired()
+		}
+	}()
+
 }
